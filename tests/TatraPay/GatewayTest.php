38413e3dfe19d710c59622cf0f23ff49ad1d2999
Tomas Majer <tomasmajer@gmail.com>
2024-10-21 21:52:37 +0200

    updated tests

--- tests/TatraPay/GatewayTest.php
+++ tests/TatraPay/GatewayTest.php
@@ -11,7 +11,7 @@ class GatewayTest extends GatewayTestCase
      */
     protected $gateway;
 
-    public function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
         $this->gateway = new Gateway($this->getHttpClient(), $this->getHttpRequest());
